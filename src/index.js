43c1bc7e34bcbb55231ed20f17278d39f1b87418
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2022-03-03 06:01:11 +0200

    Made the functionality of the img lines

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import gsap from "gsap";
 const images = {};
 
 function importAll(r) {
@@ -16,10 +17,35 @@ imgContainer.src = images[Object.keys(images)[0]]["default"];
 function createImgLine() {
   let line = document.createElement("div");
   line.classList.add("line");
-  line.id = `line-${lineContainer.children.length + 1}`;
+  // line.id = `line-${lineContainer.children.length + 1}`;
+  line.addEventListener("click", function () {
+    if (!line.classList.contains("active")) {
+      let index = Array.from(lineContainer.children).indexOf(line);
+      for (let i = 0; i < lineContainer.children.length; i++) {
+        lineContainer.children[i].classList.remove("active");
+      }
+      line.classList.add("active");
+      gsap.to(imgContainer.parentNode, { duration: 0.5, x: -800, opacity: 0 });
+      setTimeout(() => {
+        imgContainer.src = images[Object.keys(images)[index]]["default"];
+      }, 500);
+      setTimeout(() => {
+        gsap.fromTo(
+          imgContainer.parentNode,
+          { x: 300, opacity: 0 },
+          {
+            duration: 0.5,
+            x: 0,
+            opacity: 1,
+          }
+        );
+      }, 700);
+    }
+  });
   lineContainer.appendChild(line);
 }
 createImgLine();
 createImgLine();
 createImgLine();
 createImgLine();
+lineContainer.children[0].classList.add("active");
